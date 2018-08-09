0726a43067f0fa497d8bd92454a077526e04930e
jamesgallagher432 <james@hystartup.com>
2018-08-09 03:57:35 -0700

    feat: Create createProject mutation

---
+++ app/graphql/resolvers/create_project.rb
@@ -0,0 +1,99 @@
+class Resolvers::CreateProject < GraphQL::Function
+  # arguments passed as "args"
+  argument :title, !types.String
+  argument :slug, !types.String
+  argument :status, !types.String
+  argument :description, !types.String
+  argument :caption, !types.String
+  argument :milestones, !types.String
+  argument :repo_url, !types.String
+  argument :category, !types.String
+  argument :feature_image, !types.String
+  argument :project_url, !types.String
+  argument :technical_information, !types.String
+
+	description 'This function allows a user to create a portfolio project.'
+
+  # return type from the mutation
+  type Types::Projectype
+
+  # the mutation method
+  # _obj - is parent object, which in this case is nil
+  # args - are the arguments passed
+  # _ctx - is the GraphQL context (which would be discussed later)
+  def call(_obj, args, ctx)
+
+    project = Project.find_by(slug: args[:slug]).first
+
+    if args[:title].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "title_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:slug].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "slug_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:status].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "status_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:description].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "description_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:caption].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "caption_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:milestones].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "milestones_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:repo_url].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "repo_url_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:category].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "category_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:feature_image].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "feature_image_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:project_url].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "project_url_field" } )
+	    ctx.add_error(error)
+    end
+    if args[:technical_information].blank?
+      error = GraphQL::ExecutionError.new("This field is required.", options: { field: "technical_information_field" } )
+	    ctx.add_error(error)
+    end
+
+    if project.present?
+      error = GraphQL::ExecutionError.new("This slug has been used.", options: { field: "slug_field" } )
+	    ctx.add_error(error)
+    end
+
+    if error.present?
+      raise GraphQL::ExecutionError.new(ctx.errors)
+    end
+
+    Project.create!(
+      is_open: args[:is_open],
+      title: args[:title],
+      slug: args[:slug],
+      status: args[:status],
+      description: args[:description],
+      caption: args[:caption],
+      milestones: args[:milestones],
+      repo_url: args[:repo_url],
+      category: args[:category],
+      feature_image: args[:feature_image],
+      project_url: args[:project_url]
+      technical_information: args[:technical_information]
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+  end
+end

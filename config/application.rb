ba8cc260d8ffa1bacd161d3bf6d8f2241b036018
jamesgallagher432 <james@hystartup.com>
2018-08-10 05:10:20 -0700

    feat: Disable CORS

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module PortfolioApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins `*`
+
+        resource '/graphql',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

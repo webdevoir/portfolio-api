ee779e985070af69deded881317aff1e82c533bd
jamesgallagher432 <james@hystartup.com>
2018-08-13 12:29:04 -0700

    docs: Add database structure files

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,8 +5,8 @@ Types::UserType = GraphQL::ObjectType.define do
   field :name, !types.String, description: 'Name of the user'
   field :email, !types.String, description: 'Email of the user'
   field :bio, !types.String, description: 'Bio of the user'
+  field :confirmed, !types.Boolean, description: 'Confirmed status of the user'
   field :admin, !types.Boolean, description: 'Admin status of the user'
-  field :password_digest, !types.String, description: 'User password digest'
   field :profile_picture, !types.String, description: 'Profile picture of the user'
   field :password_reset, !types.String, description: 'Password reset token for users'
 end
